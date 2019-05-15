cfea5f3866e6ba3807d414abaf99e5e7fad44401
Nicholas Pung <nicholaspung>
2019-05-15 08:37:57 -0700

    WIP: used forEach to create new instances of Article

--- Article/Article.js
+++ Article/Article.js
@@ -3,9 +3,9 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector(".expandbutton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -18,12 +18,15 @@ class Article {
   }
 }
 
-/* START HERE: 
+// START HERE: 
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+// - Select all classes named ".article" and assign that value to the articles variable.  
+const article = document.querySelectorAll(".article");
+// - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList 
+// and create a new instance of Article by passing in each article as a parameter to the Article class.
+let articles = [];
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-*/
-
-let articles;
+article.forEach((e, i) => {
+  console.log(e);
+  articles[i] = new Article(e);
+});
